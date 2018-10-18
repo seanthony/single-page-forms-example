a0f0694bc441866a242b58dcbb3ab6b5756feea1
seanthony <sean@basecampcodingacademy.org>
2018-10-18 18:21:15 -0500

    form validation first attempt working, about to add another email check, then i'll be doing a hide/show for the whole site!

--- javascript.js
+++ javascript.js
@@ -18,38 +18,60 @@ function validateVotingForm(form) {
     var name = form.inputName.value.trim();
     if (name.length === 0) {
         formErrors.push({ selector: 'inputName', text: 'Please enter a name.', class: 'is-invalid', smallClass: 'invalid-feedback' });
-    } else if (name.length < 3) {
-        formCorrect.push({ selector: 'inputName', text: 'Seems like a short name.', class: '', smallClass: 'text-muted' })
+    } else if (name.length <= 3) {
+        formCorrect.push({ selector: 'inputName', text: 'Seems like a short name.', class: 'is-valid', smallClass: 'text-muted' })
     } else {
-        formCorrect.push({ selector: 'inputName', text: '', class: 'is-valid', smallClass: 'text-success' })
+        formCorrect.push({ selector: 'inputName', text: 'Thank you', class: 'is-valid', smallClass: 'text-success' })
     }
 
     var email = form.inputEmail.value.trim();
     if (email.length === 0) {
         formErrors.push({ selector: 'inputEmail', text: 'Please enter a valid email.', class: 'is-invalid', smallClass: 'invalid-feedback' });
-    } else if (email.index('@') < 0) {
+    } else if (email.indexOf('@') < 0) {
         formErrors.push({ selector: 'inputEmail', text: 'Emails must include @.', class: 'is-invalid', smallClass: 'invalid-feedback' })
+    } else if (email.length < 5) {
+        formErrors.push({ selector: 'inputEmail', text: 'Email is too short.', class: 'is-invalid', smallClass: 'invalid-feedback' })
     } else {
-        formCorrect.push({ selector: 'inputEmail', text: '', class: 'is-valid', smallClass: 'text-success' })
+        formCorrect.push({ selector: 'inputEmail', text: '', class: 'is-valid', smallClass: 'valid-feedback' })
     }
 
     var dog = form.dogSelect.value;
     if (dog.length === 0) {
         formErrors.push({ selector: 'dogSelect', text: 'Please choose the best dog in the whole world.', class: 'is-invalid', smallClass: 'invalid-feedback' });
     } else {
-        formCorrect.push({ selector: 'dogSelect', text: 'Thanks for voting!', class: 'is-valid', smallClass: 'text-success' })
+        formCorrect.push({ selector: 'dogSelect', text: 'Thanks for voting!', class: 'is-valid', smallClass: 'valid-feedback' })
     }
 
-    return { formErrors, formCorrect };
+    var text = form.explainWhy.value.trim();
+    if (text.length === 0) {
+        formErrors.push({ selector: 'explainWhy', text: 'Please explain why the dog you selected above is the best dog in the world.', class: 'is-invalid', smallClass: 'invalid-feedback' });
+    } else if (text.split(' ').length <= 3) {
+        formErrors.push({ selector: 'explainWhy', text: "C'mon, you'll need to give more than a three word answer.", class: 'is-invalid', smallClass: 'invalid-feedback' });
+    } else {
+        formErrors.push({ selector: 'explainWhy', text: " ", class: 'is-valid', smallClass: 'valid-feedback' });
+    }
+
+    return { formErrors: formErrors, formCorrect: formCorrect, obj: { name: name, email: email, dog: dog, text: text } };
 }
 
 function submitForm(event) {
     event.preventDefault(); // added so the page doesn't refresh on form submit
     var form = event.target;
+    var inputs = document.querySelectorAll('.form-control');
+    for (var input of inputs) {
+        let cls = ['is-valid', 'is-invalid'];
+        input.classList.remove(...cls);
+    }
+    var helpText = document.querySelectorAll('.form-text');
+    for (var smallText of helpText) {
+        let cls = ['valid-feedback', 'invalid-feedback'];
+        smallText.classList.remove(...cls);
+    }
     var formNotes = validateVotingForm(form);
     if (formNotes.formErrors.length === 0) {
-
+        showFinal(formNotes.obj);
     } else {
+        console.log(formNotes.formErrors);
         var feedback = formNotes.formErrors.concat(formNotes.formCorrect);
         for (var obj of feedback) {
             var element = document.querySelector('#' + obj.selector);
@@ -64,6 +86,16 @@ function submitForm(event) {
 function addFormListener() {
     var form = document.querySelector('#voting-form');
     form.addEventListener('submit', submitForm);
+}
+
+// update final text
+function showFinal(obj) {
+    for (var key in obj) {
+        var val = obj[key];
+        var selector = '#' + key;
+        var element = document.querySelector(selector);
+        element.innerHTML = val;
+    }
 }
 
 // add listeners
